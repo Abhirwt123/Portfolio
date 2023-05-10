53c3849c0c2f05bfe50ffa1b71e98c1ee9a70406
Abhirwt123 <pardeeprawat1617@gmail.com>
2023-05-10 12:13:02 +0530

    modified files

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ var typed = new Typed("#typing", {
     backSpeed: 150,
     loop: true,
   });
-  
+
        // function for cursor animation
 
   var cursor = document.getElementById("cursor");
@@ -19,3 +19,30 @@ var typed = new Typed("#typing", {
     offset: 200, 
     duration: 1000,
   });
+
+  // popup function 
+
+  let popup =document.querySelector("#popup");
+  let sendBtn =document.querySelector(".send-message");
+  let popupBtn=document.querySelector(".submit-btn");
+
+  sendBtn.addEventListener("click" , function(e){
+     e.preventDefault();
+    setTimeout(() => {
+      popup.style.display="block";
+    }, 500);
+  })
+
+  popupBtn.addEventListener("click" , function(){
+    popup.style.display="none";
+  })
+
+// function for menu bar
+
+const menuIcon =document.querySelector(".menu");
+const navbar =document.querySelector(".mobile-navbar");
+
+menuIcon.addEventListener("click",function(){
+  navbar.style.display="block";
+  navbar.classList.add("mobile-fun")
+})
