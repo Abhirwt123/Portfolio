9e3e3e6e0d0a5b804d3373fafaabed3c4c7f3cdc
Abhirwt123 <pardeeprawat1617@gmail.com>
2023-10-19 12:27:07 +0530

    make array of projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,14 +51,59 @@ window.addEventListener("scroll", () => {
     header.classList.remove("header-fixed");
   }
 });
-
+const products = [
+  {
+    id: 1,
+    image: "../assets/images/project1.png",
+    name: "Kitchn Landing Page",
+    detail: "This is a landing page using Html5,Scss,javascript.",
+    link: "https://abhirwt123.github.io/Kitchn_Landing_page/",
+  },
+  {
+    id: 2,
+    image: "../assets/images/rb-logo.jpg",
+    name: "Resume Genratore",
+    detail:
+      "This is a  html,css and javascript project made during the session of web devlopment course.",
+    link: "https://abhirwt123.github.io/Resume-genrator/",
+  },
+  {
+    id: 3,
+    image: "../assets/images/Project-manager-calendar-apps.png",
+    name: "Calender",
+    detail:
+      "This is a javascript project made during the session of web devlopment course.",
+    link: "https://abhirwt123.github.io/Calender/",
+  },
+  {
+    id: 4,
+    image: "../assets/images/notes.jpg",
+    name: "Notes Taking Project",
+    detail: "This is a landing page using Html5,Scss,javascript.",
+    link: "https://abhirwt123.github.io/Notes_taking_project/",
+  },
+  {
+    id: 5,
+    image: "../assets/images/ImageGallery.jpg",
+    name: "Image Gallery Project",
+    detail:
+      "In this project i used html, Sass and javascript and a pexels API for image render",
+    link: "https://abhirwt123.github.io/Image_gallery/",
+  },
+  {
+    id: 6,
+    image: "../assets/images/starbucks.jpg",
+    name: "Starbucks UI",
+    detail:
+      "In this project i used html, Sass and javascript and it is fully responsive for all devices",
+    link: "https://abhirwt123.github.io/StarbucksUI/",
+  },
+];
 // fetch products data from local json
 const fetchProductsList = async () => {
   const parent = document.querySelector(".projects-info");
-  const list = await fetch("../assets/js/projects.JSON");
-  const res = await list.json();
 
-  const allData = res.forEach((list) => {
+  products.forEach((list) => {
     const createDom = `
      <img
        src=${list.image}
